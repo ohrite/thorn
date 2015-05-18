1cf2b8864528d86056a08a3a6b83037e4478c78b
Doc Ritezel <utpair+doc@usertesting.com>
2015-05-17 19:55:49 -0700

    Add test coverage reporter

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 group :development, :test do
   gem 'rspec', '~> 3.0'
 end
+
+group :test do
+  gem 'codeclimate-test-reporter', require: nil
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
+
 require 'thorn'
